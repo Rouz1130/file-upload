b129c2f9c360292cf4440d75176a26929190eda1
unknown <rmajlessi@gmail.com>
2018-03-01 02:11:59 -0800

    Re Add csv-tojson file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works';
+export class AppComponent implements OnInit {
+filesData = [];
+selected;
+uploadedCount:number;
 
-  constructor() {
+  constructor(private dataService: DataService) { }
 
+  ngOnInit() {
+    this.uploadedCount = this.dataService.filesData.length;
+    this.filesData = this.dataService.filesData;
+    this.selected = this.filesData[0];
   }
+
+  uploaded(data:Array<any>) {
+    this.dataService.filesData.push(data);
+    this.uploadedCount = this.dataService.filesData.length;
+  }
+
+  onFileChange(val:number) {
+    this.selected = this.filesData[val];
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 // import { RouterModule, Routes } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
+
+import { CSVtoJSON } from './csv-to-json';
+import { DataService } from './services/data/data.service';
+
+
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
   ],
-  providers: [],
+  providers: [CSVtoJSON,DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
