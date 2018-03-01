252ee042fe35eca0eaea13b0bb92858be4cc2177
unknown <rmajlessi@gmail.com>
2018-03-01 00:38:15 -0800

    Add file upload component

---
+++ src/app/components/file-upload/file-upload.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+// import { CSVtoJSON } from '../../csv-to-json';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
