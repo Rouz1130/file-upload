53a1d0412cc3cbeea0be1e71595c0c0872cbc6f9
unknown <rmajlessi@gmail.com>
2018-03-01 16:19:56 -0800

    Add D3 dependencies, Add simple D3 4 testr in root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import * as D3 from 'd3/Index';
 import { CSVtoJSON } from './csv-to-json';
 
 @Component({
@@ -6,10 +7,26 @@ import { CSVtoJSON } from './csv-to-json';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  errorMessage: string;
+  host;
+  svg;
     
-  constructor() {
-      
+  constructor(private _element: ElementRef) {
+    this.host = D3.select(this._element.nativeElement);
   }
+
+  ngOnInit() {
+    this.buildSVG();
+  }
+
+  buildSVG() {
+    this.host.html('');
+    this.svg = this.host.append('svg')
+      .attr('width', '600')
+      .attr('height', '400')
+      .style('background-color', 'blue');
+  }
+
+
 }
